a580d0997cf70ef5e5301d4153ccdbfc889a154f
stephen <skaranja654@gmail.com>
2023-08-14 12:43:15 +0300

    feat: config.py

---
+++ server/config.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/saka-keja2.0'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'saka-keja-key'
+
+    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
