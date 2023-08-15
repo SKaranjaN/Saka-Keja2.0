5ceb36d4fabee4fadb3cd946c60ec7c4181e50cb
stephen <skaranja654@gmail.com>
2023-08-15 15:00:28 +0300

    owner's greeting

--- client/src/compononents/OwnerDashboard.js
+++ client/src/compononents/OwnerDashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PostProperty from './PostProperty';
 import ViewProperties from './ViewProperties';
@@ -9,6 +9,32 @@ import '../styles/TenantDashboard.css';
 function OwnerDashboard() {
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState('post-property');
+  const [userData, setUserData] = useState(null); // State for owner's data
+
+  useEffect(() => {
+    // Fetch owner's data and store it in state
+    const fetchUserData = async () => {
+      const userId = localStorage.getItem('user_id');
+      if (!userId) {
+        console.error('User ID not found in local storage.');
+        return;
+      }
+
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/users/${userId}`);
+        const data = await response.json();
+        if (data && data.first_name) {
+          setUserData(data);
+        } else {
+          console.error('User data not in the expected format.');
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
@@ -54,19 +80,26 @@ function OwnerDashboard() {
               Settings
             </div>
           </li>
-          <li>
-            
-          </li>
         </ul>
         <button
-              className="logout-button"
-              onClick={handleLogout}
-            >
-              Logout
-            </button>
+          className="logout-button"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
       </div>
 
       <div className="tenant-dashboard-content">
+      <div className="greeting" style={{ padding: '20px' }}>
+          {userData && (
+            <p>
+              <span role="img" aria-label="Waving Hand Emoji">
+                👋
+              </span>
+              Hi, {userData.first_name}
+            </p>
+          )}
+        </div>
         {activeTab === 'post-property' && <PostProperty />}
         {activeTab === 'view-properties' && <ViewProperties />}
         {activeTab === 'view-payment-balance' && <ViewPaymentBalance />}
