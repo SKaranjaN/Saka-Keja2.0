ae4109d6dafd5f91d6ae1c2ac4c7d52e6f2b6ec3
stephen <skaranja654@gmail.com>
2023-08-21 22:27:06 +0300

    daraja api

--- client/src/compononents/Payments.js
+++ client/src/compononents/Payments.js
@@ -1,49 +1,27 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import '../styles/Payments.css';
 
 function Payments() {
-  const navigate = useNavigate();
   const [amount, setAmount] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
-  const [transactionId, setTransactionId] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
-  console.log(navigate)
+  const [successMessage, setSuccessMessage] = useState('');
 
   const handlePayment = async (event) => {
     event.preventDefault();
 
-    const accessToken = localStorage.getItem('access_token');
-
-    const currentDate = new Date();
-    const paymentDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)
-      .toString()
-      .padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`;
-
-    const paymentData = {
-      amount: parseFloat(amount),
-      status: 'paid',
-      tenant_id: 2,
-      property_id: 1,
-      payment_date: paymentDate,
-    };
-
     try {
-      const response = await fetch('http://127.0.0.1:5000/payments', {
+      const response = await fetch(`http://127.0.0.1:5000/payments?phoneNumber=${phoneNumber}&amount=${amount}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
         },
-        body: JSON.stringify(paymentData),
       });
-
-      const data = await response.json();
 
       if (response.ok) {
         setSuccessMessage('Your payment is successful!');
-        setTransactionId(data.order_id);
-        setPhoneNumber(data.owner_phone_number);
+        
       } else {
+        const data = await response.json();
         alert(`Payment failed: ${JSON.stringify(data)}`);
       }
     } catch (error) {
@@ -53,21 +31,33 @@ function Payments() {
 
   return (
     <div className="payments-container">
-      <h2>Make a Payment</h2>
       {successMessage && (
         <div className="success-message">
           <p>{successMessage}</p>
-          <p>Your transaction ID: {transactionId}</p>
-          <p>Payment was done to phone number: {phoneNumber}</p>
         </div>
       )}
-      <form onSubmit={handlePayment}>
+      <form className="payments-form" onSubmit={handlePayment}>
+        <div className="mpesa-image">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/M-PESA_LOGO-01.svg/1200px-M-PESA_LOGO-01.svg.png"
+          alt="Mpesa"
+          style={{ width: '130px', height: 'auto', margin: '0px 0' }}
+        />
+        </div>
         <div className="payment-input">
-          <label>Amount:</label>
+          <input
+            type="text"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            placeholder="Phone number start with 254"
+          />
+        </div>
+        <div className="payment-input">
           <input
             type="number"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
+            placeholder='Amount'
           />
         </div>
         <button className="payment-button" type="submit">
